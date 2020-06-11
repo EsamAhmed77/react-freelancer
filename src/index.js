91b705c72260654f7b20acccef9f6034475ead20
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-06-11 16:22:41 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// global style
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './components/style/Main.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
