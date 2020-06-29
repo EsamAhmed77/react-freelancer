3f3abc0ce78fcaeec96e5dfb0b8ac2210ce75a5d
esam <esam18ahmed@gmail.com>
2020-06-30 00:24:34 +0200

    Adding the Fade-carousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import container from "react-bootstrap/Container";
+import React from "react";
+import Container from "react-bootstrap/Container";
 import NavBar from "./Navbar/NavBar";
 import "./style/Main.scss";
+import OurCarousel from "./Carousel/OurCarousel";
 
 function App() {
   return (
-    <container fluid>
-      <NavBar/>
-    </container>
+    <Container fluid className="fixing-padding">
+      <NavBar />
+      <OurCarousel />
+    </Container>
   );
 }
 
