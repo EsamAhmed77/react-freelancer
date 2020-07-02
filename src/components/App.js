94521140c2ac686addf37c1c8828fd3ef853422f
esam <esam18ahmed@gmail.com>
2020-07-02 02:21:16 +0200

    improving the carousel and adding the Companies component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import Container from "react-bootstrap/Container";
 import NavBar from "./Navbar/NavBar";
 import "./style/Main.scss";
 import OurCarousel from "./Carousel/OurCarousel";
+import Companies from "./Companies/Companies";
 
 function App() {
   return (
     <Container fluid className="p-0">
       <NavBar />
       <OurCarousel />
+      <Companies />
     </Container>
   );
 }
