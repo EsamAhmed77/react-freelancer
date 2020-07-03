b4ee40938fcaff3cb0f7b3f37ccc6ba3c55478f6
esam <esam18ahmed@gmail.com>
2020-07-03 06:13:08 +0200

    Fixing the companies layout and some problems with the responsive carousel

--- src/components/Companies/Companies.js
+++ src/components/Companies/Companies.js
@@ -7,58 +7,63 @@ import Col from "react-bootstrap/Col";
 
 const Companies = () => {
   return (
-    <div className="companies">
-      <Container>
-        <div className="bot_border">
-          <Row>
-            <div className="col-md-auto col-sm-12 company_span">
-              <span>As used by</span>
-            </div>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/amazon_logo2.svg"
-                alt="amazon"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/facebook-corporate-logo2.svg"
-                alt="facebook"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/deloitte-logo2.svg"
-                alt="deloitte"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/novo-nordisk-logo2.svg"
-                alt="novo"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/nasa-logo2.svg"
-                alt="nasa"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/ibm-logo2.svg"
-                alt="ibm"
-              />
-            </Col>
-            <Col>
-              <img
-                src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/airbus-logo2.svg"
-                alt="airbus"
-              />
-            </Col>
-          </Row>
+    <div className="our_container p-0
+    ">
+      <Row className="m-0">
+        <div className="companies">
+          <Col className="company_span col-md-auto">
+            <span>As used by</span>
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/amazon_logo2.svg"
+              alt="amazon"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/companies/microsoft.svg"
+              alt="microsoft"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/facebook-corporate-logo2.svg"
+              alt="facebook"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/deloitte-logo2.svg"
+              alt="deloitte"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/novo-nordisk-logo2.svg"
+              alt="novo"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/nasa-logo2.svg"
+              alt="nasa"
+            />
+          </Col>
+          <Col >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/ibm-logo2.svg"
+              alt="ibm"
+            />
+          </Col>
+          <Col className="" >
+            <img
+              src="https://www.f-cdn.com/assets/main/en/assets/home/redesign/companies/airbus-logo2.svg"
+              alt="airbus"
+            />
+          </Col>
         </div>
-      </Container>
+      </Row>
     </div>
   );
 };
